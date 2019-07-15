dba45cfec0191558fa808515b28828b40627d934
Jamaal Riggins <jamaalariggins@gmail.com>
2019-07-15 19:28:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var name = "Joe"
+
+if (name === "Joe"){
+  console.log("The name is correct")
+}
 var height = "74"
+if(height === "74"){
+  console.log("The height is correct")
+}
 
 // Don't worry about this
 module.exports = { name, height
